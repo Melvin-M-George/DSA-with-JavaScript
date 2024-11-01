c8d7aac0ede0367b65164255977d331cc68ed34e
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-11-01 20:25:29 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,33 @@
+//Fibonacci sequence
+// let arr = [0,1];
+// function fibonacci(n){
+//     let first = 0;
+//     let second = 1;
+//     for(let i=2;i<n;i++){
+//         let ans = first + second;
+//         arr.push(ans);
+
+//         first = second;
+//         second = ans;
+//     }
+// }
+// fibonacci(7);
+// console.log(arr);
+
+
+//Factorial of an integer
+// function factorial(n){
+//     let fact = 1;
+//     for(let i=2;i<=n;i++){
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+// console.log(factorial(5));
+
+
+//prime number or not
+function isPrime(n){
+
+}
+
