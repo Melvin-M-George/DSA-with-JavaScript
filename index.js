9b067b1e1af87c5f7eab15e7f1f38375100d6b0b
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-20 18:22:41 +0530

    first n elements of the fibonacci series

--- index.js
+++ index.js
@@ -1,74 +1,14 @@
-//Fibonacci sequence
-// let arr = [0,1];
+//find first n elements of the fibonacci sequnce
+
 // function fibonacci(n){
-//     let first = 0;
-//     let second = 1;
-//     for(let i=2;i<n;i++){
-//         let ans = first + second;
-//         arr.push(ans);
-
-//         first = second;
-//         second = ans;
-//     }
-// }
-// fibonacci(7);
-// console.log(arr);
-
-
-//Factorial of an integer
-// function factorial(n){
-//     let fact = 1;
-//     for(let i=2;i<=n;i++){
-//         fact = fact * i;
-//     }
-//     return fact;
-// }
-// console.log(factorial(5));
-
-
-//prime number or not
-// function isPrime(n){
-//     if(n < 2){
-//         return false;
-//     }
-//     for(let i=2;i<= Math.sqrt(n);i++){
-//         if(n % i == 0){
-//            return false
-//         }
-//     }
-//     return true;
-// }
-// console.log(isPrime(5));
-
-
-//check a number is power of two or not
-// function isPowerOfTwo(n){
-//     if(n < 1){
-//         return false;
-//     }
-//     while(n > 1){
-//         if(n % 2 !== 0){
-//             return false;
-//         }
-//         n = n / 2;
-//     }
-//     return true;
-   
+//    const fib = [0,1];
+//    for(i=2;i<n;i++){
+//     fib[i] = fib[i-1] + fib[i-2];
+//    }
+//    return fib;
 // }
 
-// console.log(isPowerOfTwo(14));
+// console.log(fibonacci(7));
 
+//big-o = O(n)
 
-//hash map
-
-// let map = new Map();
-
-// map.set('a',1);
-// map.set('b',1);
-// map.set('c',3);
-// map.set("c",4);
-
-// console.log(map.size);
-// console.log(map.get("c"));
-
-// console.log(map);
