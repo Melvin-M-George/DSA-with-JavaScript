80f8038ea59fbb72018bdfe0711c0f6660c04d8a
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-22 13:18:12 +0530

    Recursive factorial of a number

--- index.js
+++ index.js
@@ -72,3 +72,33 @@
 
 //Big-O = O(log n);
 
+
+
+//Recursive fibonacci- nth fibonacci number
+
+// function recursivefibonacci(n){
+//     if(n < 2){
+//         return n;
+//     }
+//     return recursivefibonacci(n-1) + recursivefibonacci(n-2);  
+// }
+// console.log(recursivefibonacci(0));
+// console.log(recursivefibonacci(1));
+// console.log(recursivefibonacci(6));
+
+
+//Big-o = O(2^n);
+
+//Recursive factorial of a number
+
+// function recursivefactorial(n){
+//     if(n === 0){
+//         return 1;
+//     }
+//     return recursivefactorial(n-1) * n;
+// }
+
+// console.log(recursivefactorial(4));
+
+//Big-O = O(n);
+
