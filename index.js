768cde16f0f6d7dcb27493d725fb622a1aca7152
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-23 10:44:44 +0530

    binary search

--- index.js
+++ index.js
@@ -117,3 +117,28 @@
 // console.log(linearSearch(a,5));
 
 //Big-O = O(n)
+
+
+//Binary search
+
+// function binarySearch(arr,target){
+//     let start = 0;
+//     let end = arr.length-1;
+
+//     while(start <= end){
+//         let mid = Math.floor((start + end)/2);
+//         if(target === arr[mid]){
+//             return mid;
+//         }else if(target < arr[mid]){
+//             end = mid - 1;
+//         }else{
+//             start = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// console.log(binarySearch([-5,2,4,6,10],-5));
+
+//Big-O = O(log n)
+
