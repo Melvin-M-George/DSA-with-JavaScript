6b504c380cff5c92de22186679ec3b5a0c11f4ec
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-26 11:14:07 +0530

    new files added

---
+++ Search.js
@@ -0,0 +1,40 @@
+
+//Linear seach
+
+// function linearSearch(arr,t){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i] === t){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// let a = [1,2,3,4,5];
+// console.log(linearSearch(a,5));
+
+//Big-O = O(n)
+
+
+//Binary search
+
+// function binarySearch(arr,target){
+//     let start = 0;
+//     let end = arr.length-1;
+
+//     while(start <= end){
+//         let mid = Math.floor((start + end)/2);
+//         if(target === arr[mid]){
+//             return mid;
+//         }else if(target < arr[mid]){
+//             end = mid - 1;
+//         }else{
+//             start = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// console.log(binarySearch([-5,2,4,6,10],-5));
+
+//Big-O = O(log n)
---
+++ linkedlist.js
@@ -0,0 +1,44 @@
+//Linked list
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    prepend(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    
+}
+
+
+const list = new LinkedList();
+console.log(list.isEmpty());
+console.log(list.getSize());
+list.prepend(19);
+list.prepend(22);
+list.prepend(33); 
