355800d221a270d49d4d2bba7acb04d60b9247fe
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-30 10:15:31 +0530

    reverse a linked list

--- practiceQuestions.js
+++ practiceQuestions.js
@@ -132,11 +132,21 @@ class LinkedList {
         return -1;
     }
 
-    
+    reverse(){
+        let prev = null;
+        let curr = this.head;
+        while(curr){
+            let next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        this.head = prev;
+    }
 }
 
 const list = new LinkedList();
 list.append(1);
 list.prepend(0);
 list.append(2);
-list.print();
+list.print();s
