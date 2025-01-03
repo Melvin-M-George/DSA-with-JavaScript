5da8b2512325692e3cae8ba75a4c8ec20f557c05
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-03 19:24:40 +0530

    insertion sort implemented

--- Sorting.js
+++ Sorting.js
@@ -16,6 +16,26 @@ function bubbleSort(arr) {
 }
 
 
-const arr = [22,33,55,44,11];
-bubbleSort(arr);
-console.log(arr);
+// const arr = [22,33,55,44,11];
+// bubbleSort(arr);
+// console.log(arr);
+
+
+//Insertion Sort
+
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let numberToInsert = arr[i];
+        let j = i-1;
+        while(j >= 0 && arr[j] > numberToInsert){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = numberToInsert;
+    }
+}
+
+
+// const arr = [77,22,33,66,11,55,44];
+// insertionSort(arr);
+// console.log(arr);
