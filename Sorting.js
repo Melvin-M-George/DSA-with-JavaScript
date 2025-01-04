2d25b82685f8f0562c9e038b924023b0e672a111
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-04 11:21:03 +0530

    quick sort implementation

--- Sorting.js
+++ Sorting.js
@@ -39,3 +39,28 @@ function insertionSort(arr){
 // const arr = [77,22,33,66,11,55,44];
 // insertionSort(arr);
 // console.log(arr);
+
+
+//Quick Sort
+
+function quickSort(arr){
+    if(arr.length < 2){
+        return arr;
+    }
+    let pivot = arr[arr.length-1];
+    let left = [];
+    let right = [];
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const arr = [33,55,44,11,22];
+
+console.log(quickSort(arr));
--- temp.js
+++ temp.js
@@ -1,15 +1,20 @@
-function insertionSort(arr){
-    for(let i=1;i<arr.length;i++){
-        let numberToInsert = arr[i];
-        let j = i - 1;
-        while(j >= 0 && arr[j] > numberToInsert){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = numberToInsert;
+function quickSort(arr){
+    if(arr.length < 2){
+        return arr;
     }
+    let pivot = arr[arr.length-1];
+    let left = [];
+    let right = [];
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-const arr = [11,55,44,22,33];
-insertionSort(arr);
-console.log(arr);
+
+const arr = [33,11,22,55,44];
+console.log(quickSort(arr));
