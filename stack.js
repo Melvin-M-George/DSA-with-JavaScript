5e94691c9d076a9ab44ed0f507d5eb5c33f1dd5b
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-07 10:28:37 +0530

    stack implementation using linked list

--- stack.js
+++ stack.js
@@ -38,12 +38,60 @@ class Stack{
     }
 }
 
-const stack  = new Stack();
+// const stack  = new Stack();
+// stack.push(11);
+// stack.push(22);
+// stack.push(33);
+// stack.push(44);
+// stack.push(55);
+// console.log(stack.peek());
+// console.log(stack.size());
+// console.log(stack.printStack());
+
+
+//Implementation of stack using linked list
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack1{
+    constructor(){
+        this.top = null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return this.size === 0;
+    }
+
+    push(value){
+        const node = new Node(value);
+        node.next = this.top;
+        this.top = node;
+        this.size++;
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return "Stack is empty";
+        }
+        const poppedValue = this.top.value;
+        this.top = this.top.next;
+        this.size--;
+        return poppedValue;
+    }
+
+    peek(){
+        if(this.isEmpty()) return "stack is empty";
+        return this.top.value;
+    } 
+}
+
+const stack = new Stack1();
 stack.push(11);
 stack.push(22);
 stack.push(33);
-stack.push(44);
-stack.push(55);
-console.log(stack.peek());
-console.log(stack.size());
-console.log(stack.printStack());
