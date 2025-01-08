86cda37f5702cbd44ebf45877e53ed2b1e57deef
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-08 20:00:55 +0530

    implementation of selection sort

--- Sorting.js
+++ Sorting.js
@@ -2,17 +2,17 @@
 
 function bubbleSort(arr) {
     let swapped;
-    do{
+    do {
         swapped = false;
-        for(let i=0;i<arr.length-1;i++){
-            if(arr[i] > arr[i+1]){
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
                 let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
                 swapped = true;
             }
         }
-    }while(swapped);
+    } while (swapped);
 }
 
 
@@ -23,15 +23,15 @@ function bubbleSort(arr) {
 
 //Insertion Sort
 
-function insertionSort(arr){
-    for(let i=1;i<arr.length;i++){
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
         let numberToInsert = arr[i];
-        let j = i-1;
-        while(j >= 0 && arr[j] > numberToInsert){
-            arr[j+1] = arr[j];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = numberToInsert;
+        arr[j + 1] = numberToInsert;
     }
 }
 
@@ -43,17 +43,17 @@ function insertionSort(arr){
 
 //Quick Sort
 
-function quickSort(arr){
-    if(arr.length < 2){
+function quickSort(arr) {
+    if (arr.length < 2) {
         return arr;
     }
-    let pivot = arr[arr.length-1];
+    let pivot = arr[arr.length - 1];
     let left = [];
     let right = [];
-    for(let i=0;i<arr.length-1;i++){
-        if(arr[i] < pivot){
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
             left.push(arr[i]);
-        }else{
+        } else {
             right.push(arr[i]);
         }
     }
@@ -68,34 +68,56 @@ function quickSort(arr){
 
 //Merge Sort
 
-function mergeSort(arr){
-    if(arr.length < 2){
+function mergeSort(arr) {
+    if (arr.length < 2) {
         return arr;
     }
 
     const mid = Math.floor(arr.length / 2);
-    const leftArr = arr.slice(0,mid);
+    const leftArr = arr.slice(0, mid);
     const rightArr = arr.slice(mid);
 
-    return merge(mergeSort(leftArr),mergeSort(rightArr));
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
-function merge(leftArr,rightArr){
+function merge(leftArr, rightArr) {
     let sortedArr = [];
-    while(leftArr.length && rightArr.length){
-        if(leftArr[0] <= rightArr[0]){
+    while (leftArr.length && rightArr.length) {
+        if (leftArr[0] <= rightArr[0]) {
             sortedArr.push(leftArr.shift());
-        }else{
+        } else {
             sortedArr.push(rightArr.shift())
         }
     }
 
-     return [...sortedArr, ...leftArr, ...rightArr];
+    return [...sortedArr, ...leftArr, ...rightArr];
 }
 
 
-const arr = [33,44,55,11,22];
-console.log(mergeSort(arr));
+// const arr = [33,44,55,11,22];
+// console.log(mergeSort(arr));
 
 
 //Big-O = O(n log n)
+
+
+//Selection sort
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+// const arr = [33, 55, 44, 11, 22];
+// selectionSort(arr);
+// console.log(arr);
