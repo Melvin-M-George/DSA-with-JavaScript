58ba2fa1266c87b8b1156ce4c56c4e8451dcc071
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-13 19:41:44 +0530

    Binary tree traversal - DFS preorder traversal

--- PracticeTemp.js
+++ PracticeTemp.js
@@ -1,60 +1,61 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
     }
 }
 
-class BinarySearchTree{
-    constructor(){
+
+class BinarySearchTree {
+    constructor() {
         this.root = null;
     }
 
-    isEmpty(){
+    isEmpty() {
         return this.root === null;
     }
 
-    insert(value){
+    insert(value) {
         const node = new Node(value);
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
             this.root = node;
-        }else{
-            this.insertNode(this.root,node);
+        } else {
+            this.insertNode(this.root, node);
         }
     }
 
-    insertNode(root,node){
-        if(node.value < root.value){
-            if(root.left === null){
+    insertNode(root, node) {
+        if (node.value < root.value) {
+            if (root.left === null) {
                 root.left = node;
-            }else{
+            } else {
                 this.insertNode(root.left, node);
             }
-        }else{
-            if(root.right === null){
+        } else {
+            if (root.right === null) {
                 root.right = node;
-            }else{
-                this.insertNode(root.right,node);
+            } else {
+                this.insertNode(root.right, node);
             }
         }
     }
 
-    search(root,value){
-        if(!root){
+    search(root, value) {
+        if (!root) {
             return false;
-        }else{
-            if(root.value === value){
+        } else {
+            if (value === root.value) {
                 return true;
-            }else if(value < root.value){
+            } else if (value < root.value) {
                 return this.search(root.left, value);
-            }else{
-                return this.search(root.right,value);
+            } else {
+                return this.search(root.right, value);
             }
         }
     }
 
-    preOrder(root){
+    preOrder(root) {
         if(root){
             console.log(root.value);
             this.preOrder(root.left);
@@ -64,11 +65,12 @@ class BinarySearchTree{
 }
 
 const bst = new BinarySearchTree();
-
 bst.insert(11);
 bst.insert(22);
 bst.insert(33);
-bst.insert(44);
-bst.insert(55);
+bst.insert(11);
+bst.insert(11);
+
+
 
 bst.preOrder(bst.root);
