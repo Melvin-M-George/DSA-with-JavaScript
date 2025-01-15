180f4d6b30f5f8f97f95873daea6fdedfed0540d
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-15 18:33:46 +0530

    adjacency list in graph

--- Graph.js
+++ Graph.js
@@ -4,4 +4,13 @@ const matrix = [
     [0,1,0]
 ]
 
-console.log(matrix[0][2]);
+// console.log(matrix[0][2]);
+
+
+const adjacencyList = {
+    "A":["B"],
+    "B":["A","C"],
+    "C":["B"]
+}
+
+console.log(adjacencyList["B"]);
