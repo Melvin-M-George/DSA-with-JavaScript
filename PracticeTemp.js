ccaf161dfb625172bcf3cc18d2949eae175cc070
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-01-17 19:34:19 +0530

    practice heapify

--- PracticeTemp.js
+++ PracticeTemp.js
@@ -1,62 +1,43 @@
-class Graph{
+class MinHeap{
     constructor(){
-        this.adjList = {};
+        this.heap = [];
     }
 
-    addVertex(vertex){
-        if(!this.adjList[vertex]){
-            this.adjList[vertex] = new Set();
+    getParentIndex(i){
+        return Math.floor((i-1)/2);
+    }
+
+    getLeftChildIndex(i){
+        return (2*i) + 1;
+    }
+
+    getRightChildIndex(i){
+        return (2*i) + 2;
+    }
+
+    swap(i,j){
+        [this.heap[i],this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+
+    insert(value){
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    heapifyUp(){
+        let index = this.heap.length-1;
+        while(index > 0){
+            let parentIndex = this.getParentIndex(index);
+            if(this.heap[parentIndex] > this.heap[index]){
+                this.swap(parentIndex,index);
+                index = parentIndex;
+            }else{
+                break;
+            }
         }
     }
 
-    addEdge(vertex1, vertex2){
-        if(!this.adjList[vertex1]){
-            this.addVertex(vertex1);
-        }
-
-        if(!this.adjList[vertex2]){
-            this.addVertex(vertex2);
-        }
-
-        this.adjList[vertex1].add(vertex2);
-        this.adjList[vertex2].add(vertex1);
-    }
-
-    removeEdge(vertex1,vertex2){
-        this.adjList[vertex1].delete(vertex2);
-        this.adjList[vertex2].delete(vertex1);
-    }
-
-    removeVertex(vertex){
-        if(!this.adjList[vertex]){
-            return;
-        }
-        for(let adjVertex of this.adjList[vertex]){
-            this.removeEdge(vertex,adjVertex);
-        }
-        delete this.adjList[vertex]
-    }
-
-    hasEdge(vertex1,vertex2){
-        return this.adjList[vertex1].has(vertex2) &&
-        this.adjList[vertex2].has(vertex1);
-    }
-
-    display(){
-        for(let vertex in this.adjList){
-            console.log(vertex + "->" + [...this.adjList[vertex]]);
-        }
+    remove(){
+        
     }
 }
-
-const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-
-graph.addEdge("A","B");
-graph.addEdge("B","C");
-
-
-
-graph.display();
